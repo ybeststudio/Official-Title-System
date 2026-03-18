7a761bda36aae5913aa701a39c772cc8aa254da5
Best Studio <yakupp6@gmail.com>
2026-03-18 22:09:25 +0300

    New Version

---
+++ 1.Svn/DumpProto/ItemCSVReader.cpp
@@ -0,0 +1,18 @@
+
+// en başa define tanımla
+#define ENABLE_TITLE_SYSTEM
+
+// Ara
+
+		,"USE_PUT_INTO_RING_SOCKET"
+
+
+// Değiştir
+
+#ifdef ENABLE_TITLE_SYSTEM
+		,"USE_PUT_INTO_RING_SOCKET"
+		,"USE_TITLE"
+#else
+		,"USE_PUT_INTO_RING_SOCKET"
+#endif
+
